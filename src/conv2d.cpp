d3064768fa6e09f41f9ca31f0cd56124cd91a0e7
Koki Mitsunami <k.mitsunami@gmail.com>
2025-03-24 22:09:16 +0000

    Revert back not to use NEON to avoid errors

--- src/conv2d.cpp
+++ src/conv2d.cpp
@@ -4,9 +4,6 @@
 #ifdef _OPENMP
 #include <omp.h>
 #endif
-#ifdef __aarch64__
-#include <arm_neon.h>
-#endif
 
 Tensor conv2d_general(const Tensor& input, const Tensor& weights, const Tensor& bias,
                       const std::vector<int>& strides, const std::vector<int>& pads, const std::vector<int>& dilations, int groups) {
@@ -45,11 +42,7 @@ Tensor conv2d_general(const Tensor& input, const Tensor& weights, const Tensor& 
                 int oc_index = g * group_oc + oc;
                 for (int oh = 0; oh < OH; ++oh) {
                     for (int ow = 0; ow < OW; ++ow) {
-#ifdef __aarch64__
-                        float32x4_t acc = vdupq_n_f32(0.0f);
-#else
                         float sum = 0.0f;
-#endif
                         for (int ic = 0; ic < group_ic; ++ic) {
                             int ic_index = g * group_ic + ic;
                             for (int kh = 0; kh < KH; ++kh) {
@@ -59,22 +52,12 @@ Tensor conv2d_general(const Tensor& input, const Tensor& weights, const Tensor& 
                                     if (ih >= 0 && ih < IH && iw >= 0 && iw < IW) {
                                         int in_idx = n * IC * in_c_stride + ic_index * in_c_stride + ih * IW + iw;
                                         int w_idx = oc_index * group_ic * w_c_stride + ic * w_c_stride + kh * KW + kw;
-#ifdef __aarch64__
-                                        float32x4_t in_val = vld1q_dup_f32(&in_data[in_idx]);
-                                        float32x4_t w_val  = vld1q_dup_f32(&w_data[w_idx]);
-                                        acc = vmlaq_f32(acc, in_val, w_val);
-#else
                                         sum += input.data()[in_idx] * weights.data()[w_idx];
-#endif
                                     }
                                 }
                             }
                         }
-#ifdef __aarch64__
-                        float result = vgetq_lane_f32(acc, 0) + vgetq_lane_f32(acc, 1) + vgetq_lane_f32(acc, 2) + vgetq_lane_f32(acc, 3);
-#else
                         float result = sum;
-#endif
                         int out_idx = n * OC * out_c_stride + oc_index * out_c_stride + oh * OW + ow;
                         output.data()[out_idx] = result + bias.data()[oc_index];
                     }
@@ -115,11 +98,7 @@ Tensor conv2d_pointwise(const Tensor& input, const Tensor& weights, const Tensor
         for (int oc = 0; oc < OC; ++oc) {
             for (int oh = 0; oh < OH; ++oh) {
                 for (int ow = 0; ow < OW; ++ow) {
-#ifdef __aarch64__
-                    float32x4_t acc = vdupq_n_f32(0.0f);
-#else
                     float sum = 0.0f;
-#endif
                     for (int ic = 0; ic < IC; ic+=4) {
                         for (int j = 0; j < 4 && (ic + j) < IC; ++j) {
                             int ih = oh * SH - PH;
@@ -127,21 +106,11 @@ Tensor conv2d_pointwise(const Tensor& input, const Tensor& weights, const Tensor
                             if (ih >= 0 && ih < IH && iw >= 0 && iw < IW) {
                                 int in_idx = n * IC * in_c_stride + (ic + j) * in_c_stride + ih * IW + iw;
                                 int w_idx = oc * IC + ic + j;
-#ifdef __aarch64__
-                                float32x4_t in_val = vld1q_dup_f32(&input.data()[in_idx]);
-                                float32x4_t w_val  = vld1q_dup_f32(&weights.data()[w_idx]);
-                                acc = vmlaq_f32(acc, in_val, w_val);
-#else
                                 sum += input.data()[in_idx] * weights.data()[w_idx];
-#endif
                             }
                         }
                     }
-#ifdef __aarch64__
-                    float result = vaddvq_f32(acc);
-#else
                     float result = sum;
-#endif
                     int out_idx = n * OC * out_c_stride + oc * out_c_stride + oh * OW + ow;
                     output.data()[out_idx] = result + bias.data()[oc];
                 }
@@ -185,11 +154,7 @@ Tensor conv2d_depthwise(const Tensor& input, const Tensor& weights, const Tensor
         for (int c = 0; c < C; ++c) {
             for (int oh = 0; oh < OH; ++oh) {
                 for (int ow = 0; ow < OW; ++ow) {
-#ifdef __aarch64__
-                    float32x4_t acc = vdupq_n_f32(0.0f);
-#else
                     float sum = 0.0f;
-#endif
                     for (int kh = 0; kh < KH; ++kh) {
                         for (int kw = 0; kw < KW; ++kw) {
                             int ih = oh * SH - PH + kh * DH;
@@ -197,21 +162,11 @@ Tensor conv2d_depthwise(const Tensor& input, const Tensor& weights, const Tensor
                             if (ih >= 0 && ih < IH && iw >= 0 && iw < IW) {
                                 int in_idx = n * C * in_c_stride + c * in_c_stride + ih * IW + iw;
                                 int w_idx = c * w_c_stride + kh * KW + kw;
-#ifdef __aarch64__
-                                float32x4_t in_val = vld1q_dup_f32(&input.data()[in_idx]);
-                                float32x4_t w_val = vld1q_dup_f32(&weights.data()[w_idx]);
-                                acc = vmlaq_f32(acc, in_val, w_val);
-#else
                                 sum += input.data()[in_idx] * weights.data()[w_idx];
-#endif
                             }
                         }
                     }
-#ifdef __aarch64__
-                    float result = vaddvq_f32(acc);
-#else
                     float result = sum;
-#endif
                     int out_idx = n * C * out_c_stride + c * out_c_stride + oh * OW + ow;
                     output.data()[out_idx] = result + bias.data()[c];
                 }
